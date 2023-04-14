0a9e11599d3b10d0d6a202c12f7ba208a42b5432
Jonathan Phair <jmphair@gmail.com>
2023-04-14 12:06:53 -0400

    edit: rename file

---
+++ cms/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return "Hi about page";
+});
+
+Route::get('/contact', function () {
+    return "Contact!";
+});
+
+Route::get('/post/{id}', function ($id) {
+    return "This is post number ".$id;
+});
+
+Route::get('/post/{id}/{name}', function ($id, $name) {
+    return "This is post number ".$id." by ".$name;
+});
